b0231de347b3056bba60dbca57f543c32d48ea0f
chester <chesterhsu0830@gmail.com>
2025-11-25 02:13:02 +0800

    web1125

--- web____________ (2).sql
+++ web____________ (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-11-23 17:16:45
+-- 產生時間： 2025-11-24 19:12:38
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -33,6 +33,17 @@ CREATE TABLE `checkins` (
   `checkin_time` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- 傾印資料表的資料 `checkins`
+--
+
+INSERT INTO `checkins` (`id`, `resident_id`, `checkin_time`) VALUES
+(3, 1, '2025-11-24 16:57:08'),
+(4, 4, '2025-11-24 17:08:28'),
+(5, 2, '2025-11-24 17:08:34'),
+(6, 4, '2025-11-24 17:08:44'),
+(7, 5, '2025-11-24 17:19:15');
+
 -- --------------------------------------------------------
 
 --
@@ -57,7 +68,8 @@ INSERT INTO `residents` (`id`, `student_id`, `name`, `room`, `phone`, `email`, `
 (1, '413401235', '許小策', '101', '0968635830', NULL, '2025-11-18 05:53:18'),
 (2, '413401508', '張底齊', '102', '091234567', NULL, '2025-11-18 06:07:30'),
 (3, '413401340', '沉思與', '103', '090000000', NULL, '2025-11-18 07:19:06'),
-(4, '413401234', '咪咪', '104', '0987654321', NULL, '2025-11-23 15:46:42');
+(4, '413401234', '咪咪', '104', '0987654321', NULL, '2025-11-23 15:46:42'),
+(5, '41344321', '林阿信', '105', '0931231653', NULL, '2025-11-24 17:16:51');
 
 -- --------------------------------------------------------
 
@@ -81,7 +93,12 @@ INSERT INTO `violations` (`id`, `resident_id`, `violation`, `points`, `created_a
 (1, 2, '在宿舍抽菸引發大火', 999, '2025-11-18 06:41:29'),
 (2, 2, '用菸蒂燙牆壁引發大火', 999, '2025-11-18 06:44:01'),
 (3, 2, '把菸蒂彈到停車場引發大火', 999, '2025-11-18 06:44:35'),
-(4, 2, '邊騎車邊抽菸引發大火', 999, '2025-11-18 07:00:27');
+(4, 2, '邊騎車邊抽菸引發大火', 999, '2025-11-18 07:00:27'),
+(6, 2, '邊騎車邊抽菸彈菸蒂到警察車上引發大火', 999, '2025-11-24 17:11:06'),
+(7, 4, '打麻將隕石自摸把桌子打破，造成整棟宿舍的二樓變成一樓', 999, '2025-11-24 17:12:18'),
+(9, 1, '太帥', 4, '2025-11-24 17:18:12'),
+(10, 2, '邊騎車邊抽菸彈菸蒂到警察車上引發大火', 999, '2025-11-24 17:42:01'),
+(11, 4, 'test', 1, '2025-11-24 17:45:07');
 
 --
 -- 已傾印資料表的索引
@@ -115,19 +132,19 @@ ALTER TABLE `violations`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `checkins`
 --
 ALTER TABLE `checkins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `residents`
 --
 ALTER TABLE `residents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `violations`
 --
 ALTER TABLE `violations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- 已傾印資料表的限制式
