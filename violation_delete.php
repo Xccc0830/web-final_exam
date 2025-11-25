a0d4a22a3a4a3615eb24468cffcf872ccb25cd73
chester <chesterhsu0830@gmail.com>
2025-11-25 14:00:56 +0800

    web1125

--- violation_delete.php
+++ violation_delete.php
@@ -8,7 +8,7 @@ $stmt = $conn->prepare("DELETE FROM violations WHERE id=?");
 $stmt->bind_param("i", $id);
 
 if ($stmt->execute()) {
-    header("Location: violation_list.php?resident_id=$resident_id");
+    header("Location: violation_list_all.php");
     exit;
 } else {
     echo "刪除失敗: " . $stmt->error;
