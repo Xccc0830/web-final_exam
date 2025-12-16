69aedff0aa0ff003b26e5c628b6c3f40675186c2
chester <chesterhsu0830@gmail.com>
2025-12-16 12:30:17 +0800

    web1216

--- web____________ .sql
+++ web____________ .sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- 主機: 127.0.0.1
--- 產生時間： 
--- 伺服器版本: 10.1.22-MariaDB
--- PHP 版本： 7.1.4
+-- 主機： 127.0.0.1
+-- 產生時間： 2025-12-16 05:29:46
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,11 +30,11 @@ SET time_zone = "+00:00";
 CREATE TABLE `checkins` (
   `id` int(11) NOT NULL,
   `resident_id` int(11) NOT NULL,
-  `checkin_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `checkin_time` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- 資料表的匯出資料 `checkins`
+-- 傾印資料表的資料 `checkins`
 --
 
 INSERT INTO `checkins` (`id`, `resident_id`, `checkin_time`) VALUES
@@ -48,7 +47,9 @@ INSERT INTO `checkins` (`id`, `resident_id`, `checkin_time`) VALUES
 (9, 6, '2025-11-25 06:06:00'),
 (10, 5, '2025-11-25 06:06:07'),
 (11, 3, '2025-11-25 06:06:11'),
-(12, 7, '2025-11-25 06:55:37');
+(12, 7, '2025-11-25 06:55:37'),
+(13, 1, '2025-12-16 04:27:04'),
+(14, 8, '2025-12-16 04:27:07');
 
 -- --------------------------------------------------------
 
@@ -59,26 +60,28 @@ INSERT INTO `checkins` (`id`, `resident_id`, `checkin_time`) VALUES
 CREATE TABLE `residents` (
   `id` int(11) NOT NULL,
   `student_id` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `name` varchar(50) NOT NULL,
+  `role` varchar(10) NOT NULL DEFAULT 'student',
   `room` varchar(20) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- 資料表的匯出資料 `residents`
+-- 傾印資料表的資料 `residents`
 --
 
-INSERT INTO `residents` (`id`, `student_id`, `name`, `room`, `phone`, `email`, `created_at`) VALUES
-(1, '413401235', '許小策', '101', '0968635830', NULL, '2025-11-18 05:53:18'),
-(2, '413401508', '張底齊', '102', '091234567', NULL, '2025-11-18 06:07:30'),
-(3, '413401340', '沉思與', '103', '090000000', NULL, '2025-11-18 07:19:06'),
-(4, '413401234', '咪咪', '104', '0987654321', NULL, '2025-11-23 15:46:42'),
-(5, '41344321', '林阿信', '105', '0931231653', NULL, '2025-11-24 17:16:51'),
-(6, '413404564', '顏振宇', '201', '0987456197', NULL, '2025-11-25 06:03:53'),
-(7, '41340136', '王夏威', '202', '200000000000', NULL, '2025-11-25 06:54:33'),
-(8, '413401558', '其又伯夷', '203', '65464464448', NULL, '2025-11-25 07:04:49');
+INSERT INTO `residents` (`id`, `student_id`, `password`, `name`, `role`, `room`, `phone`, `created_at`) VALUES
+(1, '413401235', 'test1234', '許小策', 'student', '101', '0968635830', '2025-11-18 05:53:18'),
+(2, '413401508', 'pw', '張底齊', 'student', '102', '091234567', '2025-11-18 06:07:30'),
+(3, '413401340', 'pw', '沉思與', 'student', '103', '090000000', '2025-11-18 07:19:06'),
+(4, '413401234', 'pw', '咪咪', 'student', '104', '0987654321', '2025-11-23 15:46:42'),
+(5, '41344321', 'pw', '林阿信', 'student', '105', '0931231653', '2025-11-24 17:16:51'),
+(6, '413404564', 'pw', '顏振宇', 'student', '201', '0987456197', '2025-11-25 06:03:53'),
+(7, '41340136', 'pw', '王夏威', 'student', '202', '200000000000', '2025-11-25 06:54:33'),
+(8, '413401558', 'pw', '其又伯夷', 'student', '203', '65464464448', '2025-11-25 07:04:49'),
+(9, 'admin', 'password', '系統管理員', 'admin', '000', '0900000000', '2025-12-16 04:25:17');
 
 -- --------------------------------------------------------
 
@@ -91,11 +94,11 @@ CREATE TABLE `violations` (
   `resident_id` int(11) NOT NULL,
   `violation` varchar(255) NOT NULL,
   `points` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- 資料表的匯出資料 `violations`
+-- 傾印資料表的資料 `violations`
 --
 
 INSERT INTO `violations` (`id`, `resident_id`, `violation`, `points`, `created_at`) VALUES
@@ -120,7 +123,7 @@ INSERT INTO `violations` (`id`, `resident_id`, `violation`, `points`, `created_a
 (25, 8, '去夜店假裝很醉去得吃別人', 2147483647, '2025-11-25 07:20:41');
 
 --
--- 已匯出資料表的索引
+-- 已傾印資料表的索引
 --
 
 --
@@ -144,36 +147,39 @@ ALTER TABLE `violations`
   ADD KEY `resident_id` (`resident_id`);
 
 --
--- 在匯出的資料表使用 AUTO_INCREMENT
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
 
 --
--- 使用資料表 AUTO_INCREMENT `checkins`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `checkins`
 --
 ALTER TABLE `checkins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
 --
--- 使用資料表 AUTO_INCREMENT `residents`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `residents`
 --
 ALTER TABLE `residents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
--- 使用資料表 AUTO_INCREMENT `violations`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `violations`
 --
 ALTER TABLE `violations`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
 --
--- 已匯出資料表的限制(Constraint)
+-- 已傾印資料表的限制式
 --
 
 --
--- 資料表的 Constraints `checkins`
+-- 資料表的限制式 `checkins`
 --
 ALTER TABLE `checkins`
   ADD CONSTRAINT `checkins_ibfk_1` FOREIGN KEY (`resident_id`) REFERENCES `residents` (`id`) ON DELETE CASCADE;
 
 --
--- 資料表的 Constraints `violations`
+-- 資料表的限制式 `violations`
 --
 ALTER TABLE `violations`
   ADD CONSTRAINT `violations_ibfk_1` FOREIGN KEY (`resident_id`) REFERENCES `residents` (`id`) ON DELETE CASCADE;
